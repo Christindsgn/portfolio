fa6a5e44cddb2a7138575cc2deea0812f8798a8b
Christin Sibi <christin@Christins-MacBook-Air.local>
2025-05-18 23:50:49 +0530

    Updated design and layout improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
-  site: 'https://christinsibi.github.io',  // Replace with your GitHub username
+  site: 'https://christinsibi.in',
+  base: '/',
+  output: 'static',
   integrations: [react(), mdx(), tailwind()]
 });
